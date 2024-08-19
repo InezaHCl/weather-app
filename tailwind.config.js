03b676cdf1f3364e0c3c54eacb9d84ceb3c78691
inezaHCl <inezaherve8@gmail.com>
2024-08-19 11:41:48 +0530

    add initila code and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        white: "#fff",
+        blackText: "#222",
+        primary: "#13ce66",
+        gray: "#8492a6",
+        gray_dark: "#273444",
+        gray_light: "#d3dce6",
       },
     },
   },
